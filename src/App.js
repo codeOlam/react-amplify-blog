a0177346d35d078883e84941085595ece230ac8f
Frank UN <frankuchenna30@gmail.com>
2021-07-05 21:30:06 +0100

    modified App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World!</h1>
+        <h2> This is a clear indication that I can now do CI/CD</h2>
       </header>
     </div>
   );
